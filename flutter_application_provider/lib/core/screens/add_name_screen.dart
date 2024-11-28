6aa49821a09807aa4da84a7d0c402ac52ff102ae
Martinfir <martinfeier123@gmail.com>
2024-11-28 16:43:51 -0300

    tp

---
+++ flutter_application_provider/lib/core/screens/add_name_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../provider/names_provide.dart';
+
+class AddNameScreen extends StatelessWidget {
+  final TextEditingController _controller = TextEditingController();
+  final TextEditingController _controller1 = TextEditingController();
+
+  AddNameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Name'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                labelText: 'Enter a new name',
+              ),
+            ),
+            TextField(
+              controller: _controller1,
+              decoration: const InputDecoration(
+                labelText: 'Enter a description',
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (_controller.text.isNotEmpty) {
+                  Provider.of<NamesModel>(context, listen: false).addName(_controller.text);
+                  Navigator.of(context).pop();
+              }
+              else{
+                     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text('No new name was detected', style: TextStyle(fontSize: 20)),
+                  backgroundColor: Color.fromARGB(255, 8, 64, 110)
+            ));
+              }
+              },
+              child: const Text('Add Name'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
